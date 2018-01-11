7977c025ffd30433ceb223e9bd288dfebf3a50c8
der2030 <derrickye@hotmail.com>
2018-01-11 18:38:16 +1300

    Java8Demo version 1.0

--- src/test/java/der/java8/lambda/BookFilterTest.java
+++ src/test/java/der/java8/lambda/BookFilterTest.java
@@ -22,20 +22,14 @@ public class BookFilterTest {
 
     @Test
     public void filterTest(){
-        List<Book> bookList= Arrays.asList(new Book(1L,"Java in Action","Java",10,33.7),
-                new Book(2L,"Springboot in Action","Java",5,103.9),
-                new Book(3L,"SpringCloud in Action","Java",2,127.7),
-                new Book(4L,"Jquery in Action","JavaScript",12,23.8),
-                new Book(5L,"Angular in Action","JavaScript",10,51.6));
 
-
-        List<Book> booksByCategory=BookFilter.filterByBooks(bookList,(Book b)->"Java".equals(b.getBookcategory()));
+        List<Book> booksByCategory=BookFilter.filterByBooks(Book.bookList,(Book b)->"Java".equals(b.getBookcategory()));
         assertThat(booksByCategory,notNullValue());
         booksByCategory.stream().forEach(book -> {
             System.out.println(book.getBookcategory()+":"+book.getBookname());
         });
 
-        List<Book> booksByPrice=BookFilter.filterByBooks(bookList,(Book b)->b.getBookprice()>50);
+        List<Book> booksByPrice=BookFilter.filterByBooks(Book.bookList,(Book b)->b.getBookprice()>50);
         assertThat(booksByPrice,notNullValue());
         System.out.println("List all books that cost large than 50:");
         booksByPrice.stream().forEach(book -> {
