062af993d01885426f64584469e97588313b6914
der2030 <derrickye@hotmail.com>
2018-01-11 18:36:57 +1300

    Java8Demo version 1.0

--- src/main/java/der/java8/lambda/Book.java
+++ src/main/java/der/java8/lambda/Book.java
@@ -1,4 +1,8 @@
 package der.java8.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
 * @FileName:Book
 * @Description:
@@ -11,13 +15,15 @@ public class Book {
     private String bookcategory;
     private int bookstock;
     private double bookprice;
+    private boolean special;
 
-    public Book(Long id, String bookname, String bookcategory, int bookstock, double bookprice) {
+    public Book(Long id, String bookname, String bookcategory, int bookstock, double bookprice, boolean special) {
         this.id = id;
         this.bookname = bookname;
         this.bookcategory = bookcategory;
         this.bookstock = bookstock;
         this.bookprice = bookprice;
+        this.special = special;
     }
 
     public Long getId() {
@@ -59,4 +65,21 @@ public class Book {
     public void setBookprice(double bookprice) {
         this.bookprice = bookprice;
     }
+
+    public boolean isSpecial() {
+        return special;
+    }
+
+    public void setSpecial(boolean special) {
+        this.special = special;
+    }
+
+
+    public static final List<Book> bookList= Arrays.asList(new Book(1L,"Java in Action","Java",10,33.7,false),
+            new Book(2L,"Springboot in Action","Java",5,103.9,true),
+            new Book(3L,"SpringCloud in Action","Java",2,127.7,false),
+            new Book(4L,"Jquery in Action","JavaScript",12,23.8,false),
+            new Book(5L,"Angular in Action","JavaScript",10,51.6,true));
+
+
 }
